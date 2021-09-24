c2ab518fb4023c5602bdb77ef3aa801c9fadd4f8
Joao Vilela <jmdvilela@gmail.com>
2021-09-24 19:18:23 -0300

    wip module

--- src/SDDPDSO.jl
+++ src/SDDPDSO.jl
@@ -1,5 +1,22 @@
 module SDDPDSO
 
-greet() = print("Hello World!")
+    # --- packages
+    using PSRClassesInterface
+    using SDDP
+    using JuMP
+    # using Statistics
+    # using Libdl
+    # using CSV
+    # using DataFrames
+
+    # --- version check
+    @static if VERSION < v"1.6"
+        error("Julia version $VERSION not supported by SDDP-DSO, upgrade to 1.6 or later")
+    end
+
+    # --- includes
+    include("model.jl")
+    include("problem.jl")
+    include("statistics.jl")
 
 end # module
