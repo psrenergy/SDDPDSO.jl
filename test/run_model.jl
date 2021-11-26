a188f49933db01db732fd68929014dec31b017b1
Joao Vilela <jmdvilela@gmail.com>
2021-11-26 10:05:18 -0300

    wip run_model.jl

--- test/run_model.jl
+++ test/run_model.jl
@@ -1,1 +1,14 @@
 println("Running model")
+
+# --- build sddp problem
+m = DSO.build_model(par)
+
+# --- export subproblems
+for i in [1, 2, par.stages]
+    DSO.SDDP.write_subproblem_to_file(m[i], joinpath(x.PATH,"lp","subproblem_$i.lp"))
+end
+
+# --- set seed
+
+
+# --- rodar o modelo
