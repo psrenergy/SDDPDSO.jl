67ffb1cd07e6d175d880a2323be4e950be158b20
Joao Vilela <jmdvilela@gmail.com>
2022-09-28 16:54:38 -0300

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,56 +1,54 @@
 import SDDPDSO
+const DSO    = SDDPDSO
+const HiGHS  = DSO.HiGHS
+const Random = DSO.Random
+const PSRIO  = DSO.PSRIO
+
 using Test
-using GLPK
-using Random
-using PSRIO
 
-const DSO = SDDPDSO
+# @show psrio = PSRIO.create()
 
-using PSRIO
-@show psrio = PSRIO.create()
+# # ---
+# casepath = joinpath(".", "data", "example_00")
 
-# ---
-casepath = joinpath(".", "data", "example_00")
+# # --- initialize DSO
+# DSO.initialize(casepath)
 
-# --- initialize DSO
-DSO.initialize(casepath)
+# # ---
+# opt = HiGHS.Optimizer
 
-# ---
-opt = GLPK.Optimizer
 
-#par.flag_dem_rsp, par.flag_import, par.flag_export, par.flag_sec_law
+# Bin_set = [0,1]
+# # for a in Bin_set, b in Bin_set, c in Bin_set
+# #     println([a,b,c])
+# # end
 
-Bin_set = [0,1]
-# for a in Bin_set, b in Bin_set, c in Bin_set
-#     println([a,b,c])
-# end
+# # --- 
+# @testset "SDDP DSO" begin
 
-# --- 
-@testset "SDDP DSO" begin
+#     # --- Test 00 - 
+#     @testset "Read database parameters" begin
+#         @time include("read_database.jl")
+#     end
 
-    # --- Test 00 - 
-    @testset "Read database parameters" begin
-        @time include("read_database.jl")
-    end
+#     # --- Test 01 - 
+#     @testset "Setup problem" begin
+#         @time include("setup_problem.jl")
+#     end
 
-    # --- Test 01 - 
-    @testset "Setup problem" begin
-        @time include("setup_problem.jl")
-    end
-
-    # --- Test 03 - 
-    @testset "Run deterministic model" begin
-        @time include("run_deterministic_model.jl")
-    end
+#     # --- Test 03 - 
+#     @testset "Run deterministic model" begin
+#         @time include("run_deterministic_model.jl")
+#     end
     
-    # --- Test 02 - 
-    @testset "Run SDDP model" begin
-        @time include("run_model.jl")
-    end
+#     # --- Test 02 - 
+#     @testset "Run SDDP model" begin
+#         @time include("run_model.jl")
+#     end
 
-    # --- Test 04 - 
-    @testset "Write results" begin
-        # @time include("write_results.jl")
-    end
+#     # --- Test 04 - 
+#     @testset "Write results" begin
+#         # @time include("write_results.jl")
+#     end
 
-end
+# end
